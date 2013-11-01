a177a52813a8a9ddcf424eb30b210e196b6008c1
Richard Warburton <richard.warburton@gmail.com>
2013-11-01 14:33:09 +0000

    use jetty.xml file

--- embedded-jetty/src/main/java/com/jclarity/had_one_dismissal/Main.java
+++ embedded-jetty/src/main/java/com/jclarity/had_one_dismissal/Main.java
@@ -1,20 +1,26 @@
 package com.jclarity.had_one_dismissal;
 
 
+import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.xml.XmlConfiguration;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
-
+        XmlConfiguration config = new XmlConfiguration(new File("src/main/resources/jetty.xml").toURL());
+        config.configure(server);
+        
         WebAppContext context = new WebAppContext();
         context.setServer(server);
+        context.setExtractWAR(true);
         context.setContextPath("/");
+//        context.setInitParameter( "contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext" );
 
         ProtectionDomain protectionDomain = Main.class.getProtectionDomain();
         URL location = protectionDomain.getCodeSource().getLocation();
