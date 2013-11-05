56ffd5ca1d4fc976b897cb57d053614ee803fd8a
Richard Warburton <richard.warburton@gmail.com>
2013-11-05 17:10:05 +0000

    adds in scripts to run the different components

--- had_one_dismissal/src/main/scripts/run-war.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-java -jar had_one_dismissal-0.1.0.BUILD-SNAPSHOT.war
-
---
+++ sca-test-harness/src/main/scripts/run-test-harness.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -eu
+
+hod_host=$1
+auth_host=$2
+
+java -Djclarity.hod.host=$hod_host -DJMX_AUTH_SERVER_HOST=$auth_host -DJMX_AUTH_SERVER_PORT=1100 -jar sca-test-harness.jar
+
